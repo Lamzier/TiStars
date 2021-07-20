449d2a81601b88371971ad7c028dc86a3524fbf6
Lamzy <1255461704@qq.com>
2021-07-20 12:54:54 +0800

    version 1.0 2021年7月20日 12:54 1.写入基本功能

--- src/main/java/io/github/lamzier/tistars/TiStars.java
+++ src/main/java/io/github/lamzier/tistars/TiStars.java
@@ -4,15 +4,29 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TiStars extends JavaPlugin {
 
+    private static TiStars plugin;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        // 插件载入
+        plugin = this;
+        //实例化 plugin
+
+
+
 
     }
 
+    /**
+     * 获取实例plugin (getInstance)
+     * 用于获取服务器 plugin 实例对象
+     */
+    public static TiStars getInstance(){ return plugin; }
+
+
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        // 插件卸载
 
     }
 }
